a409a3132a2913b992957c32d85047ab462ae1c4
Mac <quocdunginfo@gmail.com>
2016-08-15 22:17:08 +0700

    Remove Angular test Script
    TODO: Shared Html resources among Modules

--- views/wpng/wpng.index.view.class.php
+++ views/wpng/wpng.index.view.class.php
@@ -16,16 +16,6 @@ class WpngIndexView extends _SharedMainView {
                 <button ng-click="changeTemplate($event, 'checkout')">Check Out</button>
                 <button ng-click="showPopup($event)">Show Popup</button>
 				<div>
-					<form class="form-horizontal">
-						<input class="form-control" type="text" id="myinput" ng-model="jqxInputValue" required placeholder="Hard Code"/>
-						<script type="text/javascript">
-							$(document).ready(function () {              
-								$("#myinput").jqxInput({});
-							});
-						</script>
-					</form>
-				</div>
-                <div>
                     <?php
                     Wpng::loadFile(WPNG_MODULE, 'helper/ui.control/ui.helper.render.class.php');
                     $form = new UIForm();
@@ -61,91 +51,14 @@ class WpngIndexView extends _SharedMainView {
                 <br>
                 <?php echo __('Msg', 'wpng') ?>
                 <br>
-                <div ng-include src="template.url"></div>
+                <div ng-include src="templateUrl2"></div>
             </div>
 		</div>
     </div>
             </div>
-			<script type="text/javascript">
-        $(document).ready(function () {
-            // the 'layout' JSON array defines the internal structure of the docking layout
-            var layout = [{
-                type: 'layoutGroup',
-                orientation: 'horizontal',
-                items: [{
-                    type: 'tabbedGroup',
-                    width: '20%',
-                    items: [{
-                        type: 'layoutPanel',
-                        title: 'Solution Explorer',
-                        contentContainer: 'SolutionExplorerPanel',
-						initContent: function () {
-                            // initialize a jqxTree inside the Solution Explorer Panel
-                            var source = [{
-                                icon: '../../images/earth.png',
-                                label: 'Project',
-                                expanded: true,
-                                items: [{
-                                    icon: '../../images/folder.png',
-                                    label: 'css',
-                                    expanded: true,
-                                    items: [{
-                                        icon: '../../images/nav1.png',
-                                        label: 'jqx.base.css'
-                                    }, {
-                                        icon: '../../images/nav1.png',
-                                        label: 'jqx.energyblue.css'
-                                    }, {
-                                        icon: '../../images/nav1.png',
-                                        label: 'jqx.orange.css'
-                                    }]
-                                }, {
-                                    icon: '../../images/folder.png',
-                                    label: 'scripts',
-                                    items: [{
-                                        icon: '../../images/nav1.png',
-                                        label: 'jqxcore.js'
-                                    }, {
-                                        icon: '../../images/nav1.png',
-                                        label: 'jqxdata.js'
-                                    }, {
-                                        icon: '../../images/nav1.png',
-                                        label: 'jqxgrid.js'
-                                    }]
-                                }, {
-                                    icon: '../../images/nav1.png',
-                                    label: 'index.htm'
-                                }]
-                            }];
-                            $('#solutionExplorerTree').jqxTree({ source: source, width: '100%', height: '100%' });
-                        }
-                    }]
-                },{
-                    type: 'layoutGroup',
-                    orientation: 'vertical',
-                    width: '80%',
-                    items: [{
-                        type: 'documentGroup',
-                        height: '100%',
-                        minHeight: '25%',
-                        items: [{
-                            type: 'documentPanel',
-                            title: 'Document 1',
-                            contentContainer: 'Document1Panel'
-                        }, {
-                            type: 'documentPanel',
-                            title: 'Document 2',
-                            contentContainer: 'Document2Panel'
-                        }]
-                    }]
-                }]
-            }];
-            $('#jqxDockingLayout').jqxDockingLayout({ width: '100%', height: '100%', layout: layout });
-        });
-    </script>
 			<script>
                 Wpng.App.controller('wpngController', function($scope) {
-                    $scope.templateUrl = Wpng.BaseUrl + '?module=wpng&controller=wpng&action=showhand';
+                    $scope.templateUrl2 = Wpng.BaseUrl + '?module=wpng&controller=wpng&action=showhand';
                 });
             </script>
 			<script>
