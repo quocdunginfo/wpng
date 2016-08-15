b5258bcaffaa79a0093f3b476d7082c396a950d9
Mac <quocdunginfo@gmail.com>
2016-08-15 22:05:06 +0700

    Add Doctrine 2

--- doctrine.php
+++ doctrine.php
@@ -1,6 +1,6 @@
 <?php
 // bootstrap.php
-require_once "plugins/doctrine2/vendor/autoload.php";
+require_once "plugins/doctrine/vendor/autoload.php";
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
@@ -21,8 +21,8 @@ $dbParams = array(
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 $entityManager = EntityManager::create($dbParams, $config);
 
-require_once "models/db/wpng.product.model.class.php";
-$product = new WpngProductModel();
-$product->name = 'p name';
+//require_once "models/db/wpng.product.model.class.php";
+//$product = new WpngProductModel();
+//$product->name = 'p name';
 //$entityManager->persist($product);
 //$entityManager->flush();
